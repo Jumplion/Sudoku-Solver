a5efa071d4a416390f0481730b4a22a25d8fd84f
Jumplion <tomerbraff@gmail.com>
2017-11-21 08:22:15 -0600

    initial commit

---
+++ SudokuSolver/SudokuSolver/Solver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+
+    class Solver
+    {
+        public int[][] map;
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    struct Cell
+    {
+
+    }
+
+    struct Column
+    {
+
+    }
+
+    struct Row
+    {
+
+    }
+
+    struct Number
+    {
+
+    }
+}
